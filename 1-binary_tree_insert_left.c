6224ad51a416ef959a7216c5bc40487ff6166359
Rashisky <rashisky007@gmail.com>
2023-02-28 16:19:21 +0100

    Insert left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -16,10 +16,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *node;
 
 	node = binary_tree_node(parent, value);
-	if (node == NULL)
+	if (parent == NULL)
 		return (NULL);
 
-	if (parent)
+	if (node)
 	{
 		if (parent->left)
 	{
