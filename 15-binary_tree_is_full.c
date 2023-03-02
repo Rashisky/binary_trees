33e24bfc871792b53f30964474b15adfc1a189c9
Rashisky <rashisky007@gmail.com>
2023-03-02 20:30:49 +0100

    Is full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -8,7 +8,17 @@
 */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	if (tree->left && tree->right)
-		return (1);
+	int l = 0;
+
+	if (tree && ((tree->left && tree->right) ||
+	(!(tree->left) && !(tree->right))))
+	{
+		if ((tree->left && tree->right))
+			l = binary_tree_is_full(tree->left) + binary_tree_is_full(tree->right);
+
+		if (l == 0 || l == 2)
+			return (1);
+		return (0);
+	}
 	return (0);
 }
